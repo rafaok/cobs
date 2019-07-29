a640cf40fe0921d4e4102058d24735d339e57bb8
rafaok <53365608+rafaok@users.noreply.github.com>
2019-07-29 10:41:33 +0700

    Update create.php

--- create.php
+++ create.php
@@ -1034,7 +1034,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; PT. Jasa Guna Cemerlang 2019</span>
+            <span>Copyright &copy;</span>
           </div>
         </div>
       </footer>
